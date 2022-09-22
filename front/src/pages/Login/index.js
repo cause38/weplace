e48d52f34258931b1f870240b5e482932c2ca96f
Seyeon <ryeon18@naver.com>
2022-09-23 00:05:46 +0900

    jsconfig page init, component init

--- front/src/pages/Login/index.js
+++ front/src/pages/Login/index.js
@@ -1,7 +1,29 @@
 import React from "react";
+import Form from "components/form";
+import { useNavigate } from "../../../node_modules/react-router-dom/dist/index";
 
 const Login = () => {
-	return <div>Login</div>;
+  const navigate = useNavigate();
+
+  const setLogin = () => {
+    navigate("/");
+  };
+
+  const setRegister = () => {
+    navigate("/join");
+  };
+
+  return (
+    <div>
+      <Form
+        title="Login"
+        userName="userName"
+        passWord="passWord"
+        setLogin={() => setLogin()}
+        setRegister={() => setRegister()}
+      />
+    </div>
+  );
 };
 
 export default Login;
