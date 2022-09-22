e51aa7533e8a24e4e92c72d66e1b1968b0ee531c
sy Park <sy.park@weballin.com>
2022-09-22 15:15:08 +0900

    page 추가 - 회원가입, 로그인, 마이페이지

--- front/src/Routes.js
+++ front/src/Routes.js
@@ -1,8 +1,12 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import "./styles/App.css";
+
 import Home from "./pages/Home";
 import Category from "./pages/Category";
+import Login from "./pages/Login";
+import Join from "./pages/Join";
+import MyPage from "./pages/MyPage";
 
 import Header from "./components/header";
 
@@ -13,6 +17,9 @@ function Switchs() {
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/category" element={<Category />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/join" element={<Join />} />
+				<Route path="/mypage" element={<MyPage />} />
 			</Routes>
 		</Router>
 	);
