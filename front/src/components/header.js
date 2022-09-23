9e622a5067206fc0b05b546d46d8f709a09b9739
jj Park <jj.park@weballin.com>
2022-09-23 09:39:50 +0900

    WEP-4 다크모드 클래스 제거

--- front/src/components/header.js
+++ front/src/components/header.js
@@ -7,18 +7,18 @@ const Header = () => {
 
 	return (
     <header>
-      <nav className="relative bg-white shadow dark:bg-gray-800">
+      <nav className="relative bg-white shadow">
         <div className="container px-6 py-4 mx-auto">
           <div className="lg:flex lg:items-center lg:justify-between">
             <div className="flex items-center justify-between">
               <div className="text-xl font-semibold text-orange-600">
-                <Link to="/" className="text-2xl font-bold text-orange-600 transition-colors duration-300 transform dark:text-white lg:text-3xl hover:text-gray-700 dark:hover:text-gray-300">
+                <Link to="/" className="text-2xl font-bold text-orange-600 transition-colors duration-300 transform lg:text-3xl hover:text-gray-700">
                   Weplace
                 </Link>
               </div>
 
               <div className="flex lg:hidden">
-                  <button type="button" className="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400" aria-label="toggle menu">
+                  <button type="button" className="text-gray-500 hover:text-gray-600 focus:outline-none focus:text-gray-600 " aria-label="toggle menu">
                       <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
                           <path strokeLinecap="round" strokeLinejoin="round" d="M4 8h16M4 16h16" />
                       </svg>
@@ -29,7 +29,7 @@ const Header = () => {
                   </button>
               </div>
             </div>
-            <div className="absolute inset-x-0 z-20 w-full px-6 py-4 transition-all duration-300 ease-in-out bg-white dark:bg-gray-800 lg:mt-0 lg:p-0 lg:top-0 lg:relative lg:bg-transparent lg:w-auto lg:opacity-100 lg:translate-x-0 lg:flex lg:items-center">
+            <div className="absolute inset-x-0 z-20 w-full px-6 py-4 transition-all duration-300 ease-in-out bg-white lg:mt-0 lg:p-0 lg:top-0 lg:relative lg:bg-transparent lg:w-auto lg:opacity-100 lg:translate-x-0 lg:flex lg:items-center">
               <div className="flex items-center mt-4 lg:mt-0 gap-3">
                 <button 
                   onClick={() => setisLogin(!isLogin)}
@@ -41,7 +41,6 @@ const Header = () => {
                   <div className="w-10 h-10 overflow-hidden border border-gray-400 rounded-full">
                     <img src={profile} className="object-cover w-full h-full" alt="avatar"/>
                   </div>
-                  <h3 className="mx-2 text-gray-700 dark:text-gray-200 lg:hidden">jj.park</h3>
                 </button>
               </div>
             </div>
