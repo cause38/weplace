8735202928d55ebec9bb911d5b0efbdecbeac5e5
sy Park <sy.park@weballin.com>
2022-09-23 09:00:49 +0900

    Login page 컨플릭 수정완료

--- front/src/pages/Login/index.js
+++ front/src/pages/Login/index.js
@@ -1,30 +1,29 @@
-import React from "react";
-<<<<<<< front/src/pages/Login/index.js
-import Form from "components/form";
-import { useNavigate } from "../../../node_modules/react-router-dom/dist/index";
+import React from 'react';
+import Form from 'components/form';
+import {useNavigate} from '../../../node_modules/react-router-dom/dist/index';
 
 const Login = () => {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const setLogin = () => {
-    navigate("/");
-  };
+    const setLogin = () => {
+        navigate('/');
+    };
 
-  const setRegister = () => {
-    navigate("/join");
-  };
+    const setRegister = () => {
+        navigate('/join');
+    };
 
-  return (
-    <div>
-      <Form
-        title="Login"
-        userName="userName"
-        passWord="passWord"
-        setLogin={() => setLogin()}
-        setRegister={() => setRegister()}
-      />
-    </div>
-  );
+    return (
+        <div>
+            <Form
+                title="Login"
+                userName="userName"
+                passWord="passWord"
+                setLogin={() => setLogin()}
+                setRegister={() => setRegister()}
+            />
+        </div>
+    );
 };
 
 export default Login;
