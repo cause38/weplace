b95d98eb4189b3ac993ab0ecd66d6e860f5a1d47
jj Park <jj.park@weballin.com>
2022-09-23 14:18:37 +0900

    WEP-10 header 디자인 수정

--- front/src/components/header.js
+++ front/src/components/header.js
@@ -1,52 +1,48 @@
 import {React, useState} from "react";
-import profile from '../assets/sample_profile.png';
 import { Link } from 'react-router-dom';
+import profile from '../assets/sample_profile.png';
 
 const Header = () => {
   const [isLogin, setisLogin] = useState(true)
+  const [isProfileOn, setIsProfileOn] = useState(true)
 
 	return (
-    <header>
-      <nav className="relative bg-white shadow">
-        <div className="container px-6 py-4 mx-auto">
-          <div className="lg:flex lg:items-center lg:justify-between">
-            <div className="flex items-center justify-between">
-              <div className="text-xl font-semibold text-orange-600">
-                <Link to="/" className="text-2xl font-bold text-orange-600 transition-colors duration-300 transform lg:text-3xl hover:text-gray-700">
-                  Weplace
-                </Link>
-              </div>
-
-              <div className="flex lg:hidden">
-                  <button type="button" className="text-gray-500 hover:text-gray-600 focus:outline-none focus:text-gray-600 " aria-label="toggle menu">
-                      <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                          <path strokeLinecap="round" strokeLinejoin="round" d="M4 8h16M4 16h16" />
-                      </svg>
-              
-                      <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                          <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-                      </svg>
-                  </button>
-              </div>
+    <header className="h-20 fixed w-full bg-white border-b z-50">
+      <div className="container px-6 py-4 mx-auto h-full">
+        <div className="relative h-full flex items-center justify-between">
+          <div className="flex items-center justify-between">
+            <div className="text-xl font-semibold text-orange-600">
+              <Link to="/" className="text-2xl font-bold text-orange-600 transition-colors duration-300 transform lg:text-3xl hover:text-orange-700">
+                Weplace
+              </Link>
             </div>
-            <div className="absolute inset-x-0 z-20 w-full px-6 py-4 transition-all duration-300 ease-in-out bg-white lg:mt-0 lg:p-0 lg:top-0 lg:relative lg:bg-transparent lg:w-auto lg:opacity-100 lg:translate-x-0 lg:flex lg:items-center">
-              <div className="flex items-center mt-4 lg:mt-0 gap-3">
-                <button 
-                  onClick={() => setisLogin(!isLogin)}
-                  className={(isLogin ? '' : 'hidden ') + 'flex items-center px-4 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-orange-400 rounded-md hover:bg-orange-500 focus:outline-none focus:ring focus:ring-orange-300 focus:ring-opacity-80'}>
-                  <img className="w-5" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAABmJLR0QA/wD/AP+gvaeTAAAB0UlEQVRIie3Wz25NYRQF8H3MBBOhoU3boAkjr+ABxESK0CdANRLepSWRiEQIrYToCzCQ0BEJRgz8G6qOSt3+DO5uetHb+91zSyddw3PWXmvvfb5vnx2xhf+EqpSIwYg4FxHHIuJoROyOiOWI+BAR7yNiNiIeVlX1cUMywxBuoaEzlnAd/b2ansF8ii7iNk5hGNuxA4dxFnfxPbkLOFHXdALLKXQfQwUxBzGdMQ2Md2s6mqYNXKqR9BX8zPiyyjGQrYKJbk3/MIdv2F8ScCMDpuuatmjNpNa1EvIkPuf1acd5gjn0ddA6lAduCQM1cv9L8HlW8qrA/F5yL2yEcV+adjTHWPIe92ycgnvxMkXftDtAec/h7XpiT9XHXBvNXfl+ofX5tp7K3gys0ep9bXhHkvN6I0w353DhRYlpcleu0/lOopP4Yv0B8kzZABlROkCsjsyZdYkFwIPUmioh92sOdrjcg+nV1Jhvd/DWCjqp+VtcrmOepiu/xePdBo9bXQSmMVwQM9LS3gYudpv0itAovqbQIu7gNA5orj47856OaW4pP1ra212la5gP4ma2rhOWMFXyTbtZbwfi9/V2T0Q0ornevovmevuoqqpPtSrcwr/CL+M4DzTsEXbEAAAAAElFTkSuQmCC" alt="login"/>
-                  <span className="mx-1">Login</span>
-                </button>
-                <button type="button" className={(isLogin ? 'hidden ' : '') + "flex items-center focus:outline-none"} aria-label="toggle profile dropdown">
-                  <div className="w-10 h-10 overflow-hidden border border-gray-400 rounded-full">
-                    <img src={profile} className="object-cover w-full h-full" alt="avatar"/>
-                  </div>
-                </button>
+          </div>
+          <div className="flex items-end gap-3">
+            <button 
+              onClick={() => setisLogin(!isLogin)}
+              className={(isLogin ? '' : 'hidden ') + 'flex items-center px-3 py-1 sm:px-4 sm:py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-orange-400 rounded-full hover:bg-orange-500 focus:outline-none focus:ring focus:ring-orange-300 focus:ring-opacity-80'}>
+              
+              <img className="w-5" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAABmJLR0QA/wD/AP+gvaeTAAAB0UlEQVRIie3Wz25NYRQF8H3MBBOhoU3boAkjr+ABxESK0CdANRLepSWRiEQIrYToCzCQ0BEJRgz8G6qOSt3+DO5uetHb+91zSyddw3PWXmvvfb5vnx2xhf+EqpSIwYg4FxHHIuJoROyOiOWI+BAR7yNiNiIeVlX1cUMywxBuoaEzlnAd/b2ansF8ii7iNk5hGNuxA4dxFnfxPbkLOFHXdALLKXQfQwUxBzGdMQ2Md2s6mqYNXKqR9BX8zPiyyjGQrYKJbk3/MIdv2F8ScCMDpuuatmjNpNa1EvIkPuf1acd5gjn0ddA6lAduCQM1cv9L8HlW8qrA/F5yL2yEcV+adjTHWPIe92ycgnvxMkXftDtAec/h7XpiT9XHXBvNXfl+ofX5tp7K3gys0ep9bXhHkvN6I0w353DhRYlpcleu0/lOopP4Yv0B8kzZABlROkCsjsyZdYkFwIPUmioh92sOdrjcg+nV1Jhvd/DWCjqp+VtcrmOepiu/xePdBo9bXQSmMVwQM9LS3gYudpv0itAovqbQIu7gNA5orj47856OaW4pP1ra212la5gP4ma2rhOWMFXyTbtZbwfi9/V2T0Q0ornevovmevuoqqpPtSrcwr/CL+M4DzTsEXbEAAAAAElFTkSuQmCC" alt="login"/>
+              <span className="mx-1">Login</span>
+            </button>
+
+            <button 
+              onClick={() => setIsProfileOn(!isProfileOn)}
+              type="button" className={(isLogin ? 'hidden ' : '') + "flex items-center focus:outline-none"}>
+              
+              <div className="w-11 h-11 overflow-hidden border-2 p-1 border-orange-400 rounded-full">
+                <img src={profile} className="object-cover w-full h-full" alt="avatar"/>
               </div>
+            </button>
+          </div>
+          <div className={(!isProfileOn ? '' : 'hidden ') + "absolute bottom-0 right-0 translate-y-full pt-2 z-30"}>
+            <div className="px-2 pt-4 pb-3 bg-white shadow-lg border rounded-md font-semibold text-gray-500 text-base">
+              <Link to="/mypage" className="block pb-2 px-4 border-b hover:text-orange-500">마이페이지</Link>
+              <button className="block w-full pt-3 px-4 hover:text-orange-500">로그아웃</button>
             </div>
           </div>
         </div>
-      </nav>
+      </div>
     </header>
   );
 };
