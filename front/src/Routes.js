cce399c4dcda572453ea701a4c13ba17ca4a05b1
sy Park <sy.park@weballin.com>
2022-09-23 14:43:53 +0900

    login page 로그인 기능 추가, join page 레이아웃 수정,  find-password page 추가

--- front/src/Routes.js
+++ front/src/Routes.js
@@ -9,24 +9,26 @@ import Join from './pages/Join';
 import MyPage from './pages/MyPage';
 import Detail from './pages/Detail';
 import Write from './pages/Write';
+import FindPassword from 'pages/FindPassword/index';
 
 import Header from './components/header';
 
 function Switchs() {
-  return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/category" element={<Category />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/join" element={<Join />} />
-        <Route path="/mypage" element={<MyPage />} />
-        <Route path="/detail" element={<Detail />} />
-        <Route path="/write" element={<Write />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/category" element={<Category />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/join" element={<Join />} />
+                <Route path="/mypage" element={<MyPage />} />
+                <Route path="/detail" element={<Detail />} />
+                <Route path="/write" element={<Write />} />
+                <Route path="/find-password" element={<FindPassword />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default Switchs;
--- front/src/components/inputBox.js
+++ front/src/components/inputBox.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import Button from './button';
 
-const InputBox = ({type, placeholder, ariaLabel}) => {
+const InputBox = ({name, type, value, onChange, placeholder, ariaLabel}) => {
     return (
-        <div className="w-full mt-4">
+        <div className="w-full mt-4 flex ">
+            {name && (
+                <label
+                    htmlFor="{name}"
+                    className="w-1/3 flex-auto pl-1 py-2 mt-2 text-slate-600 font-semibold placeholder-gray-500 bg-slate-200 border rounded-md focus:border-blue-400  focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300"
+                >
+                    {name}
+                </label>
+            )}
             <input
-                className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-md dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300"
+                className={
+                    name
+                        ? 'block w-2/3  pl-1 py-2  mt-2 ml-[-5px] text-gray-700 placeholder-gray-500 bg-white border rounded-md focus:border-blue-400 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300'
+                        : 'block w-full  pl-1 py-2  mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-md focus:border-blue-400 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300'
+                }
+                name={name}
                 type={type}
                 placeholder={placeholder}
                 aria-label={ariaLabel}
+                value={value}
+                onChange={onChange}
             />
         </div>
     );
