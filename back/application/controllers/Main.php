36cd175bbbaab65710f2c6a9c96a7aae038bc1e4
TG Kim <tg.kim@weballin.com>
2022-09-27 18:16:43 +0900

    메인페이지 최신리뷰 설정

---
+++ back/application/controllers/Main.php
@@ -0,0 +1,23 @@
+<?php
+use chriskacerguis\Restserver\RestController;
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require APPPATH . 'libraries/RestController.php';
+require APPPATH . 'libraries/Format.php';
+
+class Main extends RestController {
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->model('main_m');
+    }
+
+    public function index_get() {
+        $this->response([
+            'state' => 200,
+            'msg'   => 'OK',
+            'data'  => $this->main_m->getRecentReviews()
+        ]);
+    }
+}
+?>
---
+++ back/application/models/Main_m.php
@@ -0,0 +1,16 @@
+<?php
+class Main_m extends CI_Model {
+    function getRecentReviews() {
+        $q = $this->db->query("
+            SELECT r.idx, r.menu, r.star, s.name, r.comment, DATE(r.wdate) AS wdate
+            FROM T_review AS r
+                LEFT JOIN T_shop AS s
+                ON r.sidx = s.idx
+            ORDER BY r.wdate DESC
+            LIMIT 5
+        ");
+
+        return $q->result();
+    }
+}
+?>
