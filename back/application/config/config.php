25908a1e5fcc15f76e9cbc9bd41af68afc69df65
TG Kim <tg.kim@weballin.com>
2022-09-27 11:27:03 +0900

    base_url 재설정

--- back/application/config/config.php
+++ back/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://place.weballin.com';
+$config['base_url'] = 'http://place-api.weballin.com';
 
 /*
 |--------------------------------------------------------------------------
