57e687550dde97f05a16c4f209a4c75148c9b9c9
TG Kim <tg.kim@weballin.com>
2022-09-28 13:38:38 +0900

    파일 저장 위치 변경

--- back/application/models/Mypage_m.php
+++ back/application/models/Mypage_m.php
@@ -45,7 +45,7 @@ class Mypage_m extends CI_Model {
     function setProfileImg($idx, $fileName) {
         $this->db->where('idx', $idx);
         $this->db->update('T_user', [
-            'thumb' => UPLOAD_PATH . $fileName
+            'thumb' => UPLOAD_PATH . 'profile/' . $fileName
         ]);
     }
 
