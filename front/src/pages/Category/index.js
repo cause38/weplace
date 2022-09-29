b64893286ce8311fae82e518fdd73cc7de2eeca2
sy Park <sy.park@weballin.com>
2022-09-29 15:33:27 +0900

    category page 레이아웃 작업 중

--- front/src/pages/Category/index.js
+++ front/src/pages/Category/index.js
@@ -1,13 +1,44 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Category = () => {
-	return (
-		<div>
-			<h2>Category</h2>
-			<Link to="/">go to Home</Link>
-		</div>
-	);
+    return (
+        <>
+            <section className="category-nav">
+                <ul className="flex gap-[10px] mx-[10px] my-[5px] px-[10px]">
+                    {CATEGORY_LIST?.map(data => {
+                        const {id, name, url} = data;
+                        return (
+                            <Link
+                                to={url}
+                                className="px-[10px] py-[5px] min-w-[81px] rounded-[20px] bg-orange-400 text-white hover:bg-orange-700"
+                            >
+                                <li key={id} className="text-center">
+                                    {name}
+                                </li>
+                            </Link>
+                        );
+                    })}
+                </ul>
+            </section>
+            <section className="category flex">
+                <div>최신순</div>
+                <div>찜한가게</div>
+                <div>해쉬태그들</div>
+            </section>
+            <section>매장리스트</section>
+        </>
+    );
 };
+
+const CATEGORY_LIST = [
+    {id: 1, name: '전체보기', url: '/category'},
+    {id: 2, name: '한식', url: '/category'},
+    {id: 3, name: '중식', url: '/category'},
+    {id: 4, name: '일식', url: '/category'},
+    {id: 5, name: '양식', url: '/category'},
+    {id: 6, name: '분식', url: '/category'},
+    {id: 7, name: '아시안', url: '/category'},
+];
 
 export default Category;
--- front/src/pages/Login/index.js
+++ front/src/pages/Login/index.js
@@ -11,7 +11,6 @@ const Login = () => {
     const location = useLocation();
 
     const pathname = location.state?.pathname;
-    console.log('referrer', pathname);
 
     // 토큰
     const getToken = sessionStorage.getItem('token');
