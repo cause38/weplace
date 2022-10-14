82cd971acdb6c45946f5d4ad9b1e750971c80517
JJ Park <jj.park@weballin.com>
2022-10-14 20:04:28 +0900

    로그인 버튼 타입 submit 으로 변경

--- front/src/pages/Home/index.js
+++ front/src/pages/Home/index.js
@@ -104,7 +104,7 @@ const Home = () => {
                     >
                         {newReviewData.map(data => (
                             <SwiperSlide className="bg-white rounded-lg p-5 shadow-lg" key={data.idx}>
-                                <a href="#">
+                                <Link to="/detail">
                                     <div className="flex justify-between items-center mb-3">
                                         <span className="inline-block	text-xs p-1 px-3 bg-orange-400 text-white rounded-full">
                                             {data.menu}
@@ -116,7 +116,7 @@ const Home = () => {
                                         <h4 className="text-xl font-bold text-gray-900 truncate w-full">{data.name}</h4>
                                         <p className="truncate w-full text-gray-800">{data.comment}</p>
                                     </div>
-                                </a>
+                                </Link>
                             </SwiperSlide>
                         ))}
                     </Swiper>
