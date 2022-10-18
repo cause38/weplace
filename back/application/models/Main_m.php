3fe54d44ea125f1f8d17300746e26f089dc633a9
TG Kim <tg.kim@weballin.com>
2022-10-18 18:34:30 +0900

    최신리뷰 index 변경

--- back/application/models/Main_m.php
+++ back/application/models/Main_m.php
@@ -2,7 +2,7 @@
 class Main_m extends CI_Model {
     function getRecentReviews() {
         $q = $this->db->query("
-            SELECT r.idx, r.menu, r.star, s.name, r.comment, DATE(r.wdate) AS wdate
+            SELECT s.idx, r.menu, r.star, s.name, r.comment, DATE(r.wdate) AS wdate
             FROM T_review AS r
                 LEFT JOIN T_shop AS s
                 ON r.sidx = s.idx
