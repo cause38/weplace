6b9f6c70f53e2a64cf26cba6681fd1eb52cb084b
sy Park <sy.park@weballin.com>
2022-11-03 11:29:23 +0900

    마이페이지 반응형 적용

--- front/src/pages/MyPage/components/UserInfo.js
+++ front/src/pages/MyPage/components/UserInfo.js
@@ -16,12 +16,12 @@ const UserInfo = ({
     handleNickName,
 }) => {
     return (
-        <div className="flex flex-col justify-center items-center md:flex-row gap-8 mt-4 bg-white rounded-lg p-8 shadow-md">
+        <div className="flex flex-col justify-center items-center md:flex-row md:flex  gap-8 mt-4 bg-white rounded-lg p-5 shadow-md">
             <div>
                 <div className="w-36 h-36">
-                    <img className="w-full h-full rounded-[50%] overflow-hidden" src={userImg} />
+                    <img className="w-full h-full rounded-full overflow-hidden" src={userImg} />
                 </div>
-                <form className="h-[30px] mt-[10px]" encType="multipart.form-data">
+                <form className="h-8 mt-5" encType="multipart.form-data">
                     <input
                         id="file"
                         type="file"
@@ -35,50 +35,52 @@ const UserInfo = ({
                     <Button contents="프로필 사진 변경" onClick={handleProfileImg} />
                 </form>
             </div>
-            <div className="userInfo flex mb:mb-10 items-end w-[85%] justify-between">
-                <div className="flex flex-col justify-center w-[80%]">
-                    <div className="flex items-center">
-                        <div className="min-w-[70px] w-[20%]">아이디</div>
-                        <div className="w-full">
-                            <p className="ml-[5px]">{userId}</p>
-                        </div>
-                    </div>
-                    <div className="flex mt-[20px] items-center">
-                        <div className="min-w-[70px] w-[20%]">
-                            <p>닉네임</p>
-                        </div>
-                        {isChangeNickName ? (
+            <div className="flex-col justify-between align-center md:flex-row md:flex md:items-center">
+                <div className=" flex-col mb-10 items-center md:flex-row ">
+                    <div className="flex flex-col justify-center w-[80%] mr-7">
+                        <div className="flex items-center">
+                            <div className="min-w-[70px] w-[20%]">아이디</div>
                             <div className="w-full">
-                                <InputBox
-                                    id="nickName"
-                                    type="text"
-                                    value={nickName || ''}
-                                    ariaLabel="name"
-                                    placeholder={nickName}
-                                    onChange={setNickName}
-                                    ref={nameInput}
-                                />
+                                <p className="ml-[5px]">{userId}</p>
                             </div>
-                        ) : (
-                            <div className="w-full">
-                                <InputBox
-                                    id="nickName"
-                                    type="text"
-                                    value={nickName || ''}
-                                    ariaLabel="name"
-                                    onChange={setNickName}
-                                    readOnly="readOnly"
-                                />
+                        </div>
+                        <div className="flex mt-[20px] items-center">
+                            <div className="min-w-[70px] w-[20%]">
+                                <p>닉네임</p>
                             </div>
-                        )}
+                            {isChangeNickName ? (
+                                <div className="w-full min-w-fit">
+                                    <InputBox
+                                        id="nickName"
+                                        type="text"
+                                        value={nickName || ''}
+                                        ariaLabel="name"
+                                        placeholder={nickName}
+                                        onChange={setNickName}
+                                        ref={nameInput}
+                                    />
+                                </div>
+                            ) : (
+                                <div className="w-full w-full min-w-[180px]">
+                                    <InputBox
+                                        id="nickName"
+                                        type="text"
+                                        value={nickName || ''}
+                                        ariaLabel="name"
+                                        onChange={setNickName}
+                                        readOnly="readOnly"
+                                    />
+                                </div>
+                            )}
+                        </div>
                     </div>
                 </div>
                 {isChangeNickName ? (
-                    <div className="h-[40px] w-[20%] ml-[5%] min-w-[100px]">
+                    <div className="h-[40px] w-full min-w-[100px] md:w-5">
                         <Button contents="닉네임 변경" onClick={handleCheckedName} />
                     </div>
                 ) : (
-                    <div className="h-[40px] w-[20%] ml-[10px] min-w-[100px]">
+                    <div className="h-[40px] w-full min-w-[100px] md:w-5">
                         <Button contents="닉네임 변경" onClick={handleNickName} />
                     </div>
                 )}
