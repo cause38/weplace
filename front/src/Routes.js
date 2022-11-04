6a29d7405a5c4a861b9634bfd742081bb38389fb
jj Park <jj.park@weballin.com>
2022-11-04 18:22:17 +0900

    floating btn 수정

--- front/src/Routes.js
+++ front/src/Routes.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {useLocation, BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 import './styles/App.css';
 
@@ -10,25 +10,28 @@ import MyPage from './pages/MyPage';
 import Detail from './pages/Detail';
 import Write from './pages/Write';
 import FindPassword from 'pages/FindPassword/index';
+import FloatingBtn from './components/FloatingBtn';
 
 import Header from './components/header';
 import Footer from './components/footer';
 
 function Switchs() {
     return (
-        // <Router basename="http://place.weballin.com">
         <Router>
-            <Header />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/category/:id" element={<Category />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/join" element={<Join />} />
-                <Route path="/mypage" element={<MyPage />} />
-                <Route path="/detail/:id" element={<Detail />} />
-                <Route path="/write" element={<Write />} />
-                <Route path="/find-password" element={<FindPassword />} />
-            </Routes>
+            <div className="-mt-[50px] pb-10">
+                <FloatingBtn />
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/category/:id" element={<Category />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/join" element={<Join />} />
+                    <Route path="/mypage" element={<MyPage />} />
+                    <Route path="/detail/:id" element={<Detail />} />
+                    <Route path="/write" element={<Write />} />
+                    <Route path="/find-password" element={<FindPassword />} />
+                </Routes>
+            </div>
             <Footer />
         </Router>
     );
--- front/src/components/FloatingBtn.js
+++ front/src/components/FloatingBtn.js
@@ -2,14 +2,18 @@ import React from 'react';
 import {Link, useLocation} from 'react-router-dom';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faPenToSquare} from '@fortawesome/free-regular-svg-icons';
-import {useEffect} from 'react';
 
 const FloatingBtn = () => {
+    const {path} = useLocation();
     return (
-        <div>
+        <div
+            className={`${
+                path !== '/write' ? 'sticky' : 'hidden'
+            } write-btn z-[1000] w-[50px] h-[50px] flex justify-center items-center`}
+        >
             <Link
                 to="/write"
-                className="fixed bottom-24 right-24 w-12 h-12 flex justify-center items-center text-xl font-bold transition-colors bg-orange-400 hover:bg-orange-500 text-white rounded-full shadow-md shadow-orange-300 transition-colors"
+                className={`w-[50px] h-[50px] flex w-full h-full justify-center items-center text-xl font-bold transition-colors bg-orange-400 hover:bg-orange-500 text-white rounded-full shadow-md shadow-orange-300 transition-colors`}
             >
                 <FontAwesomeIcon icon={faPenToSquare} />
             </Link>
